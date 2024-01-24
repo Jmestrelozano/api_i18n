f089b25b9bdd5d3e5e2a8ed8194325941f0053e6
Jmestrelozano <jmestrelozano@gmail.com>
2024-01-24 12:29:22 -0500

    add api initial

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ import translation from "./db/data.json" assert { type: "json" };
 
 const app = express();
 
+const port = process.env.port | 3000;
+
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+
 app.get("/api/translations", (req, res) => {
   res
     .status(200)
     .json({ data: translation, message: "translations obtained correctly" });
 });
 
-app.listen(3000);
+app.listen(port);
